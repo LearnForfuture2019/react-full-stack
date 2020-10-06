7db56b3c31ff97501a06b1fcc715ef675f51246f
= <=>
2020-10-06 14:51:56 +0800

    配置antd-mobile

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import {Button} from 'antd-mobile'
+export default class App extends Component{
+    render(){
+        return (
+            <div>
+              <Button type='primary'>click me!</Button>
+            </div>
+        )
+    }
+}
